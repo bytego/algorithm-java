d77e2948f5ea25b0e014347b3d52306c12d3fb94
liuzongrui <zongrui.liu@outlook.com>
2018-08-05 21:05:39 +0800

    no message

--- src/main/java/com/algorithm/cjm/array/Rotate.java
+++ src/main/java/com/algorithm/cjm/array/Rotate.java
@@ -14,7 +14,7 @@ public class Rotate{
 
     public static void main(String[] args) {
 
-        int[] nums = {1,2,3,4,5,6,7};
+        int[] nums = new int[]{1,2,3,4,5,6,7};
         rotate(nums, 3);
     }
 
