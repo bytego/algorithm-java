17a8ff2396e29b103a08d204366c0f27fa4c542b
jieming <who1753@163.com>
2018-08-07 18:23:09 +0800

    fix

--- src/main/java/com/algorithm/cjm/string/Reverse.java
+++ src/main/java/com/algorithm/cjm/string/Reverse.java
@@ -5,20 +5,19 @@ package com.algorithm.cjm.string;
  * 给定一个 32 位有符号整数，将整数中的数字进行反转。
 
      示例 1:
-
      输入: 123
      输出: 321
-     示例 2:
 
+     示例 2:
      输入: -123
      输出: -321
-     示例 3:
 
+     示例 3:
      输入: 120
      输出: 21
-     注意:
 
-     假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
+     注意:
+     假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
  *
  */
 public class Reverse {
@@ -29,10 +28,11 @@ public class Reverse {
 
     public int reverse(int x) {
 
+        //求整数绝对值
         int length = 0;
         for(int i = 0; i < Integer.MAX_VALUE; i ++){
 
-            if(x < (10 ^ i)){
+            if(Math.abs(x) < (10 ^ i)){
                 length = i;
                 break;
             }
